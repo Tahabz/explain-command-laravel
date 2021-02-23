3a81d5a31e8b727e600209aba5d0d518ef381eed
Mohamed taha Baz <mobaz@e1r6p10.1337.ma>
2021-02-23 19:00:25 +0100

    add restful routes to package-manager and the index method

--- explain-command/routes/api.php
+++ explain-command/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CommandController;
+use App\Http\Controllers\PackageManagerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+//PackageManager Routes
+// Route::get('/package-manager', [PackageManagerController::class, 'index']);
+Route::resource('package-manager', PackageManagerController::class);
